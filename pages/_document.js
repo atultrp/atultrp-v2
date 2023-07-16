8bb2bf4422ac5b62a966994022c3dd03aa7033f2
Atul Tripathi <atul31815@gmail.com>
2023-07-16 12:03:44 +0530

    initial commit -> logo, favicons, fonts, colors

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Atul Tripathi - A Web developer, Freelancer, Designer.</title>
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Hi there, this is my personal website, I'm a web developer (freelancer), you can contact me if you have any type of website work." />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />

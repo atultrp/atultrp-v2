a959a4db2be9569348f5f88d5f7d20bc521647be
Atul Tripathi <atul31815@gmail.com>
2023-07-18 11:12:05 +0530

    techstack done

--- components/TechStack.js
+++ components/TechStack.js
@@ -1,11 +1,99 @@
+import { techStackData } from '@/constant/data'
 import React from 'react'
 
 const TechStack = () => {
+  const techStack = techStackData()
+  console.log(techStack)
+
+  const TitleText = ({ title }) => {
+    return (
+      <h3 className='text-xl font-bold text-gray-200 mt-4'>{title}</h3>
+    )
+  }
+
+  const TechStackCard = ({ title, icon }) => {
+    return (
+      <img src={icon} alt={title} className='mr-4 my-2' />
+    )
+  }
 
   return (
     <div className='mb-8 mt-20'>
       <h3 className='text-2xl font-semibold text-gray-300 italic'>What I work with</h3>
       <h2 className='text-5xl font-bold text-white'>Tech Stack</h2>
+
+      <TitleText title='Languages' />
+      <div className='flex flex-wrap'>
+        {techStack?.languages?.map((item, index) => {
+          return (
+            <TechStackCard key={index} title={item?.title} icon={item?.icon} />
+          )
+        })}
+      </div>
+
+      <TitleText title='Frameworks and Libraries' />
+      <div className='flex flex-wrap my-3'>
+        {techStack?.frameworks?.map((item, index) => {
+          return (
+            <TechStackCard key={index} title={item?.title} icon={item?.icon} />
+          )
+        })}
+      </div>
+
+      <TitleText title='Databases' />
+      <div className='flex flex-wrap my-3'>
+        {techStack?.databases?.map((item, index) => {
+          return (
+            <TechStackCard key={index} title={item?.title} icon={item?.icon} />
+          )
+        })}
+      </div>
+
+      <TitleText title='Design' />
+      <div className='flex flex-wrap my-3'>
+        {techStack?.design?.map((item, index) => {
+          return (
+            <TechStackCard key={index} title={item?.title} icon={item?.icon} />
+          )
+        })}
+      </div>
+
+      <TitleText title='Package Managers' />
+      <div className='flex flex-wrap my-3'>
+        {techStack?.packageManagers?.map((item, index) => {
+          return (
+            <TechStackCard key={index} title={item?.title} icon={item?.icon} />
+          )
+        })}
+      </div>
+
+      <TitleText title='IDEs/Editors' />
+      <div className='flex flex-wrap my-3'>
+        {techStack?.IDEs?.map((item, index) => {
+          return (
+            <TechStackCard key={index} title={item?.title} icon={item?.icon} />
+          )
+        })}
+      </div>
+
+      <TitleText title='VersionControl and OS' />
+      <div className='flex flex-wrap my-3'>
+        {techStack?.versionControlOS?.map((item, index) => {
+          return (
+            <TechStackCard key={index} title={item?.title} icon={item?.icon} />
+          )
+        })}
+      </div>
+
+      <TitleText title='Hosting' />
+      <div className='flex flex-wrap my-3'>
+        {techStack?.hosting?.map((item, index) => {
+          return (
+            <TechStackCard key={index} title={item?.title} icon={item?.icon} />
+          )
+        })}
+      </div>
+
     </div>
   )
 }
--- constant/data.js
+++ constant/data.js
@@ -8,4 +8,179 @@ export const navItems = () => {
 }
 
 export const techStackData = () => {
+  const techStack = {
+    languages: [
+      {
+        title: 'HTML',
+        icon: 'https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white'
+      },
+      {
+        title: 'CSS',
+        icon: 'https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white'
+      },
+      {
+        title: 'JavaScript',
+        icon: 'https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E'
+      },
+      {
+        title: 'Python',
+        icon: 'https://img.shields.io/badge/python-%2314354C.svg?style=for-the-badge&logo=python&logoColor=white'
+      },
+      {
+        title: 'Java',
+        icon: 'https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white'
+      },
+      {
+        title: 'C++',
+        icon: 'https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&ogoColor=white'
+      },
+      {
+        title: 'C',
+        icon: 'https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&ogoColor=white'
+      },
+    ],
+    frameworks: [
+      {
+        title: 'React',
+        icon: 'https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB'
+      },
+      {
+        title: 'Redux',
+        icon: 'https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white'
+      },
+      {
+        title: 'Next.js',
+        icon: 'https://img.shields.io/badge/nextjs-%23000000.svg?style=for-the-badge&logo=nextdotjs&logoColor=white'
+      },
+      {
+        title: 'React Router',
+        icon: 'https://img.shields.io/badge/react_router%20-%23CA4245.svg?style=for-the-badge&logo=react-router&logoColor=white'
+      },
+      {
+        title: 'Material UI',
+        icon: 'https://img.shields.io/badge/materialui-%230081CB.svg?style=for-the-badge&logo=material-ui&logoColor=white'
+      },
+      {
+        title: 'Bootstrap',
+        icon: 'https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white'
+      },
+      {
+        title: 'Tailwind CSS',
+        icon: 'https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white'
+      },
+      {
+        title: 'Node.js',
+        icon: 'https://img.shields.io/badge/node.js-%2343853D.svg?style=for-the-badge&logo=node.js&logoColor=white'
+      },
+      {
+        title: 'Express',
+        icon: 'https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge'
+      },
+      {
+        title: 'jQuery',
+        icon: 'https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white'
+      },
+    ],
+    databases: [
+      {
+        title: 'MongoDB',
+        icon: 'https://img.shields.io/badge/mongodb-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white'
+      },
+      {
+        title: 'MySQL',
+        icon: 'https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white'
+      }
+    ],
+    design: [
+      {
+        title: 'Figma',
+        icon: 'https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white'
+      },
+      {
+        title: 'Adobe Illustrator',
+        icon: 'https://img.shields.io/badge/adobe_illustrator-%23FF9A00.svg?style=for-the-badge&logo=adobeillustrator&logoColor=white'
+      },
+      {
+        title: 'Sketch',
+        icon: 'https://img.shields.io/badge/sketch-%23F7B500.svg?style=for-the-badge&logo=sketch&logoColor=white'
+      },
+    ],
+    packageManagers: [
+      {
+        title: 'npm',
+        icon: 'https://img.shields.io/badge/npm-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white'
+      },
+      {
+        title: 'yarn',
+        icon: 'https://img.shields.io/badge/yarn-%232187B6.svg?style=for-the-badge&logo=yarn&logoColor=white'
+      }
+    ],
+    IDEs: [
+      {
+        title: 'Visual Studio Code',
+        icon: 'https://img.shields.io/badge/visualstudiocode-%23007ACC.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white'
+      },
+      {
+        title: 'Atom',
+        icon: 'https://img.shields.io/badge/atom-%235849BE.svg?style=for-the-badge&logo=atom&logoColor=white'
+      },
+      {
+        title: 'CodeSandbox',
+        icon: 'https://img.shields.io/badge/codesandbox-%23F7B93E.svg?style=for-the-badge&logo=codesandbox&logoColor=white'
+      },
+      {
+        title: 'Code Pen',
+        icon: 'https://img.shields.io/badge/codepen-%23000000.svg?style=for-the-badge&logo=codepen&logoColor=white'
+      },
+    ],
+    versionControlOS: [
+      {
+        title: 'Git',
+        icon: 'https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white'
+      },
+      {
+        title: 'GitHub',
+        icon: 'https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white'
+      },
+      {
+        title: 'GitLab',
+        icon: 'https://img.shields.io/badge/gitlab-%23181717.svg?style=for-the-badge&logo=gitlab&logoColor=white'
+      },
+      {
+        title: 'Bitbucket',
+        icon: 'https://img.shields.io/badge/bitbucket-%230047B3.svg?style=for-the-badge&logo=bitbucket&logoColor=white'
+      },
+      {
+        title: 'Linux',
+        icon: 'https://img.shields.io/badge/linux-%23FCC624.svg?style=for-the-badge&logo=linux&logoColor=black'
+      },
+      {
+        title: 'Ubuntu',
+        icon: 'https://img.shields.io/badge/ubuntu-%23E95420.svg?style=for-the-badge&logo=ubuntu&logoColor=white'
+      },
+      {
+        title: 'Windows',
+        icon: 'https://img.shields.io/badge/windows-%230078D6.svg?style=for-the-badge&logo=windows&logoColor=white'
+      },
+    ],
+    hosting: [
+      {
+        title: 'Heroku',
+        icon: 'https://img.shields.io/badge/heroku-%23430098.svg?style=for-the-badge&logo=heroku&logoColor=white'
+      },
+      {
+        title: 'Netlify',
+        icon: 'https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=white'
+      },
+      {
+        title: 'Vercel',
+        icon: 'https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white'
+      },
+      {
+        title: 'Firebase',
+        icon: 'https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase'
+      },
+    ]
+  }
+  return techStack
 }

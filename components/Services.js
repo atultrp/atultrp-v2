4c5da1610a1832e71aef352e6cdd1b0ef10c6674
Atul Tripathi <atul31815@gmail.com>
2023-08-08 11:19:28 +0530

    minor changes

--- components/Services.js
+++ components/Services.js
@@ -2,11 +2,26 @@ import { ServicesData } from '@/constant/data'
 import ServiceCard from '@/shared/ServiceCard'
 import Accordion from '@/shared/ServiceCard'
 import { sectionHeading } from '@/shared/SharedComponents'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Fade } from 'react-awesome-reveal'
 
 const Services = () => {
   const servicesData = ServicesData()
+  const [serviceCardCount, setServiceCardCount] = useState(6)
+  const [viewMore, setViewMore] = useState(false)
+
+  useEffect(() => {
+    if (window.innerWidth < 1024) {
+      setServiceCardCount(3)
+    }
+  }, [])
+
+  const handleViewMore = () => {
+    if (window.innerWidth < 1024) {
+      !viewMore ? setServiceCardCount(6) : setServiceCardCount(3)
+      setViewMore(!viewMore)
+    }
+  }
 
   return (
     <div>
@@ -16,8 +31,8 @@ const Services = () => {
       </div>
       <div>
         <h3 className='text-xl font-semibold mt-2'>What I Offer</h3>
-        <div className='flex flex-wrap'>
-          {servicesData.map((item, index) => (
+        <div className='flex flex-wrap lg:justify-normal justify-center'>
+          {servicesData?.slice(0, serviceCardCount)?.map((item, index) => (
             <div className='my-3 lg:mr-5 sm:mr-3 mr-1 flex lg:justify-normal justify-center'>
               <Fade cascade duration={1500} triggerOnce>
                 <ServiceCard key={index} title={item.title} content={item.content} />
@@ -25,14 +40,23 @@ const Services = () => {
             </div>
           ))}
         </div>
+        <div className='flex justify-center my-4 lg:hidden'>
+          <button
+            className='bg-[#1a263d] px-4 py-2 rounded-lg text-gray-400 hover:text-white'
+            onClick={() => handleViewMore()}
+          >
+            {viewMore ? "View Less" : "View More"}
+          </button>
+        </div>
+
       </div>
-      <div>
+      <div className='mt-8'>
         <h3 className='text-xl font-semibold my-3'>Why Choose Me?</h3>
         <div className='space-y-2'>
           <p>
             I'm not just a developer, I'm your online journey partner. Through close collaboration, I understand your vision, goals, and audience. Together, we'll create a unique website that reflects your business objectives and sets you apart.
           </p>
-          <p>
+          <p className='italic font-medium'>
             Reach out to me, and let's bring your ideas to life!
           </p>
 

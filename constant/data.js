2ce62318c3ed35ca4c1725dce6b1b3e223c35128
Atul Tripathi <atul31815@gmail.com>
2023-10-09 11:05:46 +0530

    react native added

--- constant/data.js
+++ constant/data.js
@@ -81,6 +81,10 @@ export const techStackData = () => {
         title: 'jQuery',
         icon: 'https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white'
       },
+      {
+        title: 'React Native',
+        icon: 'https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB'
+      },
     ],
     databases: [
       {

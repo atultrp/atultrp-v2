7679b126e2af94a703be1b6ecf15fabe6e1e780b
Atul Tripathi <atul31815@gmail.com>
2025-11-24 13:27:50 +0530

    vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '@/styles/globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }) {
   return <div className='flex justify-center'>
     <Component {...pageProps} />
+    <Analytics />
   </div>
 }
